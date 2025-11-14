b8bd073792aeb6148c2f0e30fa0a585c8334655d
Muhammad Qasim <156399289+Muhammadqasim4@users.noreply.github.com>
2025-11-15 02:37:11 +0500

    Add files via upload

---
+++ data types/app.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version
+
+//alert (5+5) //we are using node.js not browser
+
+//Primitive Datatypes
+let userName = "Ali" // string => ""
+let age = 23    // number 2 to 53
+let isLogin = true //boolean
+let friend; // undefined 
+console.table([userName,age,isLogin,friend]);
+console.log(typeof null ) // object
+
+
+//null standalone value 
+//bigint simply big number
+//symbol is a unique value
+
+
